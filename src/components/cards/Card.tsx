a8fd41d943ffa2bb2cdffbf2ece31d8680859137
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2024-06-08 02:48:07 +0630

    omise integration

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -3,12 +3,14 @@ import VisaImg from './../../../images/svg/visa.svg';
 import DotImg from './../../../images/svg/dot.svg';
 import JcbImg from './../../../images/svg/jcb.svg';
 import MasterImg from './../../../images/svg/master.svg';
+import {Pressable} from 'react-native';
 
 interface Props {
   card: Card;
+  onPressCard: () => void;
 }
 
-export function Card({card}: Props) {
+export function Card({card, onPressCard}: Props) {
   const CardIcon = () => {
     if (card.number[0] === '4') {
       return <VisaImg />;
@@ -26,51 +28,56 @@ export function Card({card}: Props) {
   };
 
   return (
-    <Box width="100%" mt="4" p="5" borderRadius="md" bg="white" shadow="7">
-      <VStack>
-        <CardIcon />
+    <Pressable onPress={onPressCard}>
+      <Box width="100%" mt="4" p="5" borderRadius="md" bg="white" shadow="7">
+        <VStack>
+          <CardIcon />
 
-        <HStack space="4" alignItems="center">
-          <HStack>
-            <DotImg width={15} />
-            <DotImg width={15} />
-            <DotImg width={15} />
-            <DotImg width={15} />
-          </HStack>
+          <HStack space="4" alignItems="center">
+            <HStack>
+              <DotImg width={15} />
+              <DotImg width={15} />
+              <DotImg width={15} />
+              <DotImg width={15} />
+            </HStack>
 
-          <HStack>
-            <DotImg width={15} />
-            <DotImg width={15} />
-            <DotImg width={15} />
-            <DotImg width={15} />
-          </HStack>
+            <HStack>
+              <DotImg width={15} />
+              <DotImg width={15} />
+              <DotImg width={15} />
+              <DotImg width={15} />
+            </HStack>
 
-          <HStack>
-            <DotImg width={15} />
-            <DotImg width={15} />
-            <DotImg width={15} />
-            <DotImg width={15} />
-          </HStack>
+            <HStack>
+              <DotImg width={15} />
+              <DotImg width={15} />
+              <DotImg width={15} />
+              <DotImg width={15} />
+            </HStack>
 
-          <Text fontSize="md">
-            {card.number.substring(card.number.length - 4, card.number.length)}
-          </Text>
-        </HStack>
-
-        <HStack justifyContent="space-between">
-          <VStack space="3">
-            <Text color="muted.400">Name on Card</Text>
-            <Text fontWeight="400">{card.name}</Text>
-          </VStack>
-
-          <VStack space="3">
-            <Text color="muted.400">Expires</Text>
-            <Text fontWeight="400">
-              {`${card.expiration_month}/${card.expiration_year}`}
+            <Text fontSize="md">
+              {card.number.substring(
+                card.number.length - 4,
+                card.number.length,
+              )}
             </Text>
-          </VStack>
-        </HStack>
-      </VStack>
-    </Box>
+          </HStack>
+
+          <HStack justifyContent="space-between">
+            <VStack space="3">
+              <Text color="muted.400">Name on Card</Text>
+              <Text fontWeight="400">{card.name}</Text>
+            </VStack>
+
+            <VStack space="3">
+              <Text color="muted.400">Expires</Text>
+              <Text fontWeight="400">
+                {`${card.expiration_month}/${card.expiration_year}`}
+              </Text>
+            </VStack>
+          </HStack>
+        </VStack>
+      </Box>
+    </Pressable>
   );
 }
--- src/screens/CardLists/CardLists.tsx
+++ src/screens/CardLists/CardLists.tsx
@@ -1,11 +1,60 @@
-import {VStack, ScrollView, Box} from 'native-base';
+import {VStack, ScrollView, Box, useToast} from 'native-base';
 import {Card} from '../../components/cards';
 import {RootLayout} from '../../layouts';
 import useCardStore from '../../stores/useCard';
 import {NoCard} from './components';
+import Omise from 'omise-react-native';
+
+Omise.config(
+  'pkey_test_5wvisbxphp1zapg8ie6',
+  'skey_test_5wvisdjjoqmfof5npzw',
+  '2017-11-12',
+);
 
 export function CardLists() {
+  const toast = useToast();
   const {cards} = useCardStore();
+
+  const handlePressCard = async (card: Card) => {
+    const data = await Omise.createToken({
+      card: {
+        name: card.name,
+        city: 'Bangkok',
+        postal_code: 10320,
+        number: card.number,
+        expiration_month: card.expiration_month,
+        expiration_year: card.expiration_year,
+        security_code: card.security_code,
+      },
+    });
+
+    const omiseTokenId = data.id;
+
+    await Omise.createSource({
+      type: 'internet_banking_bbl',
+      amount: 500000,
+      currency: 'thb',
+    });
+
+    await createOmiseCharge(omiseTokenId);
+  };
+
+  const createOmiseCharge = async (omiseTokenId: string) => {
+    const data = await Omise.createCharge({
+      description: 'Sai Min',
+      amount: 500000, // 5,000 baht
+      currency: 'thb',
+      capture: true,
+      card: omiseTokenId,
+    });
+
+    if (data.paid) {
+      toast.show({
+        title: 'Successfully Created a Payment!',
+        placement: 'top',
+      });
+    }
+  };
 
   if (cards.length === 0) {
     return (
@@ -20,7 +69,11 @@ export function CardLists() {
       <ScrollView>
         <VStack flex={1} space="3" px="5" pb="5">
           {cards.map(card => (
-            <Card key={card.number} card={card} />
+            <Card
+              key={card.number}
+              card={card}
+              onPressCard={() => handlePressCard(card)}
+            />
           ))}
         </VStack>
       </ScrollView>
